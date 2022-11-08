f45a2a7409a51bdbf38bdd4ca339d50153aa0597
claybrimm <claybrimm@gmail.com>
2022-11-08 13:01:46 -0500

    Added enemies

--- hello-brave-new-world/src/main.rs
+++ hello-brave-new-world/src/main.rs
@@ -34,9 +34,11 @@ impl GameState for State {
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
+        let map = self.ecs.fetch::<Map>();
 
         for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            let idx = map.xy_idx(pos.x, pos.y);
+            if map.visible_tiles[idx] { ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph) }
         }
     }
 }
@@ -56,6 +58,20 @@ fn main() -> rltk::BError {
 
     let map : Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
+
+    for room in map.rooms.iter().skip(1) {
+        let (x,y) = room.center();
+        gs.ecs.create_entity()
+            .with(Position{ x, y })
+            .with(Renderable{
+                glyph: rltk::to_cp437('g'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
+            .build();
+    }
+
     gs.ecs.insert(map);
 
     gs.ecs
@@ -71,4 +87,5 @@ fn main() -> rltk::BError {
         .build();
 
     rltk::main_loop(context, gs)
+
 }
