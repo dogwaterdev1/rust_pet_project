173e255fe7281fedc3e19b924defe883439c24e7
claybrimm <claybrimm@gmail.com>
2022-11-08 13:06:53 -0500

    added multiple enemy types

--- hello-brave-new-world/src/main.rs
+++ hello-brave-new-world/src/main.rs
@@ -59,17 +59,26 @@ fn main() -> rltk::BError {
     let map : Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
 
-    for room in map.rooms.iter().skip(1) {
-        let (x,y) = room.center();
-        gs.ecs.create_entity()
-            .with(Position{ x, y })
-            .with(Renderable{
-                glyph: rltk::to_cp437('g'),
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
-            .build();
+    let mut rng = rltk::RandomNumberGenerator::new();
+for room in map.rooms.iter().skip(1) {
+    let (x,y) = room.center();
+
+    let glyph : rltk::FontCharType;
+    let roll = rng.roll_dice(1, 2);
+    match roll {
+        1 => { glyph = rltk::to_cp437('g') }
+        _ => { glyph = rltk::to_cp437('o') }
+    }
+
+    gs.ecs.create_entity()
+        .with(Position{ x, y })
+        .with(Renderable{
+            glyph: glyph,
+            fg: RGB::named(rltk::RED),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
+        .build();
     }
 
     gs.ecs.insert(map);
